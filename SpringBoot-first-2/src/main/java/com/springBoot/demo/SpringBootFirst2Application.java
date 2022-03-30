1e6e937f9fbc785339cd0370d8904d7ccf681391
tectoro <tectoro@localhost>
2022-03-30 19:23:59 +0530

    practice2

---
+++ SpringBoot-first-2/src/main/java/com/springBoot/demo/SpringBootFirst2Application.java
@@ -0,0 +1,13 @@
+package com.springBoot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirst2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirst2Application.class, args);
+	}
+
+}
